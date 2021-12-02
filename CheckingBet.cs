39a40db2e1c527e84707bd8564f756dc5a48919b
aakashpotdar <potdaraakash@gmail.com>
2021-12-02 20:09:51 +0530

    Check game win or not

---
+++ CheckingBet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    public class CheckingBet
+    {
+        static int CashPerDay = 100;
+        static int BetAmount = 1;
+             
+                
+    
+    public void checkGame()
+        {
+            CheckingBet obj = new CheckingBet();
+            Random random = new Random();
+            int GameWinLoos = random.Next(0, 2);
+
+            if (GameWinLoos == 1)
+            {
+                CashPerDay= CashPerDay+BetAmount;
+                Console.WriteLine("You won the Game");
+            }
+            else
+            {
+                CashPerDay = CashPerDay + BetAmount;
+                Console.WriteLine("You loss the Game");
+            }
+        }
+    }
+}
