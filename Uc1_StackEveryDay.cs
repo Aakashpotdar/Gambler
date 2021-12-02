1d7b7127888e5ea3970225e66166f5ed62b0647d
aakashpotdar <potdaraakash@gmail.com>
2021-12-02 19:05:57 +0530

    Add project files.

---
+++ Uc1_StackEveryDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    class Uc1_StackEveryDay
+    {
+
+        public void PlayGame()
+        {
+            int CashPerDay = 100;
+            int betAmount = 1;
+
+        }
+
+
+    }
+}
